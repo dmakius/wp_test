2159c08bec428eb4bb64182c49a634fe6b5c23ea
Daniel Makover <dmakover@gmail.com>
2018-07-02 09:53:57 -0400

    init 3

--- index.php
+++ index.php
@@ -11,7 +11,11 @@
  *
  * @var bool
  */
+
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+
+//*comment *//
