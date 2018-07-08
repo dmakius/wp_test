87e83cd2e6f0d1cb7da6711c54a61da93796b7aa
Daniel Makover <dmakover@gmail.com>
2018-07-08 17:50:47 -0400

    plugin part 1

--- wp-content/plugins/wp-cbf/includes/class-wp-cbf.php
+++ wp-content/plugins/wp-cbf/includes/class-wp-cbf.php
@@ -164,6 +164,7 @@ class Wp_Cbf {
 		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
 		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
 
+		$this->loader->add_action('admin_init', $plugin_admin, 'options_update');
 	}
 
 	/**
